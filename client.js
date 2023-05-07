4a94a49cfbef15bada4d4dd0c60f9ec5b6001a70
Javier Navarro <javierna182@gmail.com>
2023-05-06 23:11:03 -0500

    funtions

--- client.js
+++ client.js
@@ -1,6 +1,11 @@
 console.log('hello');
 
+let totalMonthly = 0;
+let totalEl = document.getElementById('total-monthly'); 
+
+
 function AddInfoDiv(event){
+    let totalMoCount = [];
 
 // prevents form from refreshing    
 event.preventDefault();
@@ -12,51 +17,28 @@ let ID = Number(document.getElementById('ID').value);
 let title = document.getElementById('title').value;
 let annualSalary = Number(document.getElementById('annualSalary').value);
 
+
 //adds tab
 document.body.innerHTML += 
-`<table>
+`<table onclick="removeDiv(event)">
 <tr>
 <td>${firstName}</td>
 <td>${lastName}</td>
 <td>${ID}</td>
 <td>${title}</td>
 <td>${annualSalary}</td>
-<td><button onclick="removeDiv(event)">Delete</button></div></td>    
+<td>Delete</td>
 </tr>
 </table>`;
 
+// <td><button onclick="removeDiv(event)">Delete</button></div></td>    
+
+// increment total monthly
 totalMonthly += 1;
-totalEl.innerHTML = `<div id="total-monthly"><h1>Total Monthly:${totalMonthly}</h1></div>`;
+totalEl.innerHTML = `<div id="total-monthly"> Total Monthly: $ ${totalMonthly}</div>`;
 
 }//used to add info
 
 function removeDiv(event){
     event.target.parentElement.remove();
 }//used to remove the div
-
-function totalCount(array) {
-    for (let annualSalary of array){
-        annualSalary += totalMonthly                  
-    }
-}//add total monthly count
-
-// function submitButtonaaaa(event) {
-//     let info= []; // global array to hold guess objects
-
-//     // prevents form from refreshing
-//     event.preventDefault();
-
-//     // increment total guesses
-//     totalMonthly += 1;
-//     totalEl.innerHTML = `<div>Total Monthly: ${totalMonthly}</div>`
-
-//     // get submitted inputs
-//     let firstName = document.getElementById('firstName').value;
-//     info.push({firstName});
-
-//     // console.log(guesses);
-
-//     scorer(guesses);
-
-//     document.getElementById('guess-form').reset();
-// }
