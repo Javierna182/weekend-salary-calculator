3e8c1231b7c0b3b6ed10919ff93a92a847da8653
Javier Navarro <javierna182@gmail.com>
2023-05-08 20:03:49 -0500

    fixed addinfodiv and created  tottalAdd function

--- client.js
+++ client.js
@@ -3,10 +3,21 @@ console.log('hello');
 let totalMonthly = 0;
 let totalEl = document.getElementById('total-monthly'); 
 
+let totalMoCount = [];
+let total = 0;
+
+function totalAdd (array){
+    for(let numbers of array){
+        total += numbers;
+        console.log(total);
+    } 
+    if (total/12 >= 20000){
+        document.querySelector('#total-monthly').className = 'red';
+    }
+}
 
 function AddInfoDiv(event){
-    let totalMoCount = [];
-
+    
 // prevents form from refreshing    
 event.preventDefault();
 
@@ -16,29 +27,36 @@ let lastName = document.getElementById('lastName').value;
 let ID = Number(document.getElementById('ID').value);
 let title = document.getElementById('title').value;
 let annualSalary = Number(document.getElementById('annualSalary').value);
-
-
+totalMoCount.push(annualSalary);
+totalAdd(totalMoCount);
+totalMoCount = [];
 //adds tab
-document.body.innerHTML += 
-`<table onclick="removeDiv(event)">
+document.querySelector('#employee-table').innerHTML += 
+`
 <tr>
 <td>${firstName}</td>
 <td>${lastName}</td>
 <td>${ID}</td>
 <td>${title}</td>
 <td>${annualSalary}</td>
-<td>Delete</td>
+<td><button onclick="removeDiv(event)">Delete</button></div></td> 
 </tr>
-</table>`;
+`;
+
+
+
+annualSalary = document.getElementById('annualSalary');
+
+annualSalary.value = '';
 
 // <td><button onclick="removeDiv(event)">Delete</button></div></td>    
 
 // increment total monthly
-totalMonthly += 1;
-totalEl.innerHTML = `<div id="total-monthly"> Total Monthly: $ ${totalMonthly}</div>`;
+totalMonthly += 0;
+totalEl.innerHTML = `<div id="total-monthly"> Total Monthly: $ ${total/12}</div>`;
 
 }//used to add info
 
 function removeDiv(event){
-    event.target.parentElement.remove();
+    event.target.parentElement.parentElement.remove();
 }//used to remove the div
